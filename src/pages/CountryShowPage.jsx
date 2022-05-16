f376ffc6bd91899d16c471fc8fb0a77174efab3d
brendanburke <bfburke33@gmail.com>
2022-05-16 12:23:35 -0400

    working on a sorting function for the home page

--- src/pages/CountryShowPage.jsx
+++ src/pages/CountryShowPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 const CountryShow = (props) => {
     const [countries, setCountries] = useState(null)
@@ -29,8 +30,9 @@ const CountryShow = (props) => {
                 <h2>{country.name.common}</h2>
                 <h3>Official Name: {country.name.official}</h3>
                 <img className="show-page-flag" src={country.flags.svg} alt={`Flag of ${country.name.common}`} />
-                <p>{country.unMember}</p>
-
+                <p>In {country.name.common} they drive on the {country.car.side} side of the road</p>
+                <p>The capital of {country.name.common} is {country.capital[0]}</p>
+                <a href={country.maps.googleMaps}>View on Google Maps</a>
             </div>
         )
     }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,24 +8,57 @@ const Home = (props) => {
     const [countries, setCountries] = useState(null)
 
     const url = 'https://restcountries.com/v3.1/all'
-
+    
     const getCountries = async () => {
         const response = await fetch(url)
         const data = await response.json()
         setCountries(data)
     }
+    
+    const sortedPopulations = []
 
+    async function  sortPopulation ()  {
+
+        const countryList = await countries
+        
+        for (let i = 0; i < countryList.length; i++) {
+            sortedPopulations.push(countryList[i].population)
+        }
+        sortedPopulations.sort((a, b) => b-a)
+    }
+
+    sortPopulation()
+    
     useEffect(() => {
         getCountries()
     }, [])
+    
 
 
 
     const loaded = () => {
+        console.log(sortedPopulations)
         return (
             <div className="home-container">
                 <h1>Home</h1>
-
+                {countries.map((country, idx) => {
+                    if(country.population === sortedPopulations[0]) {
+                        return (
+                            <div>
+                                <h4>The country with the highest population is: <Link to={`/${country.name.common}`}><h4>{country.name.common}</h4></Link> </h4>
+                                <p>{sortedPopulations[0]} people live in {country.name.common}</p>
+                            </div>
+                        )
+                    } 
+                    if (country.population === sortedPopulations[247]) {
+                        return (
+                            <div>
+                                <h4>The country with the lowest population is: <Link to={`/${country.name.common}`}><h4>{country.name.common}</h4></Link> </h4>
+                                <p>{sortedPopulations[247]} people live in {country.name.common}</p>
+                            </div>
+                        )
+                    }
+                })}
             </div>
         )
     }
@@ -33,6 +66,7 @@ const Home = (props) => {
     const loading = () => {
         return (
             <img className="loading" src="https://c.tenor.com/hlKEXPvlX48AAAAi/loading-loader.gif" alt='loading gif' />
+
         )
     }
 
