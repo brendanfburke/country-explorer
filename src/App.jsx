1cc21d9aa34e1627e0ac3e16b02f4497677f3d11
brendanburke <bfburke33@gmail.com>
2022-05-15 21:32:08 -0400

    added nav bar with home link

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Outlet } from 'react-router';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Hello World</h1>
       <Outlet />
     </div>
