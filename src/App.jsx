5dbe4f9dfb9b803942fe1dc65f04d54ea1be9259
brendanburke <bfburke33@gmail.com>
2022-05-15 21:02:41 -0400

    set up working links on home page, set up fetch function to grab API data on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home';
-import ContinentIndex from './pages/ContinentIndex';
-import CountryIndex from './pages/CountryIndex';
-import CountryShow from './pages/CountryShowPage';
-import FlagIndex from './pages/FlagIndex';
-import SubregionIndex from './pages/SubregionIndex';
+import { Outlet } from 'react-router';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <Home />
-      <ContinentIndex />
-      <CountryIndex /> 
-      <CountryShow />
-      <FlagIndex /> 
-      <SubregionIndex />
+      <Outlet />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 
 const Home = (props) => {
+
+    const [countries, setCountries] = useState(null)
+
+    const url = 'https://restcountries.com/v3.1/all'
+
+    const getCountries = async () => {
+        const response = await fetch(url)
+        const data = await response.json()
+        setCountries(data)
+    }
+
+    useEffect(() => {
+        getCountries()
+    }, [])
+
+
+
+
     return (
-        <h1>Home</h1>
+        <div className="home-container">
+            <h1>Home</h1>
+            <Link to='/countries' >
+                <h2>See All Countries</h2>
+            </Link>
+            <Link to='/continents' >
+                <h2>See All Continents</h2>
+            </Link>
+            <Link to='/subregions' >
+                <h2>See All Subregions</h2>
+            </Link>
+            <Link to='/flags' >
+                <h2>See All Flags</h2>
+            </Link>
+            <h3>{countries[0].name.common}</h3>
+        </div>
     )
 }
 
