3fd9f97888d1da7acaef2a9d1167dbdac85701f7
brendanburke <bfburke33@gmail.com>
2022-05-18 09:51:42 -0400

    fixed antarctica showpage bug, now rendering capital info more dynamically. Began working on search-bar function

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = (props) => {
+
+
+
     return (
         <div className="nav">
             <Link to='/' >
@@ -9,13 +12,17 @@ const Nav = (props) => {
             </Link>
             <Link to='/countries' >
                     <h2>See All Countries</h2>
-                </Link>
-                <Link to='/continents' >
-                    <h2>See All Continents</h2>
-                </Link>
-                <Link to='/flags' >
-                    <h2>See All Flags</h2>
-                </Link>
+            </Link>
+            <Link to='/continents' >
+                <h2>See All Continents</h2>
+            </Link>
+            <Link to='/flags' >
+                <h2>See All Flags</h2>
+            </Link>
+            <div className="search-box">
+                <input className="search-input" placeholder="Search here..." type="text" />
+                <button className="search-button">Search</button>
+            </div>
         </div>
     )
 }
